730ee53648529199ee182340b5ee384e4e25e072
wp <18888106880@163.com>
2018-10-12 09:08:45 +0800

    切换时间

--- book/job/spider_job.py
+++ book/job/spider_job.py
@@ -7,7 +7,7 @@ from book.job import task
 # 执行清理redis队列的job
 schedule.every().day.at("23:50").do(task.clean_job)
 # 执行爬虫job
-schedule.every().day.at("20:30").do(task.spider_job)
+schedule.every().day.at("00:00").do(task.spider_job)
 # 执行推送job
 schedule.every().day.at("08:00").do(task.push_every_day_books)
 
