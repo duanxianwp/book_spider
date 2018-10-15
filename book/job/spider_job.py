5ea77172378c1db7c2568642f488bbf8b795d568
wp <18888106880@163.com>
2018-10-15 22:53:21 +0800

    update

--- book/job/spider_job.py
+++ book/job/spider_job.py
@@ -7,9 +7,9 @@ from book.job import task
 # 执行清理redis队列的job
 schedule.every().day.at("23:50").do(task.clean_job)
 # 执行爬虫job
-schedule.every().day.at("00:00").do(task.spider_job)
+schedule.every().day.at("23:00").do(task.spider_job)
 # 执行推送job
-schedule.every().day.at("08:00").do(task.push_every_day_books)
+schedule.every().day.at("23:30").do(task.push_every_day_books)
 
 while True:
     schedule.run_pending()
