d05d50a57865954e145038c2e276d1c9cd451c13
wp <18888106880@163.com>
2018-10-15 22:52:27 +0800

    add 邮件推送

---
+++ book/db/mailutil.py
@@ -0,0 +1,30 @@
+import smtplib
+from email.header import Header
+from email.mime.text import MIMEText
+
+from book import settings
+
+
+def push_email(recieve_email, message):
+    try:
+        host = settings.EMAIL_URL
+        port = settings.EMAIL_PORT
+        user = settings.EMAIL_USER
+        password = settings.EMAIL_PASSWORD
+        mail_content = message
+        mail_title = '新书推送'
+
+        msg = MIMEText(mail_content, "html", 'utf-8')
+        msg["Subject"] = Header(mail_title, 'utf-8')
+        msg["From"] = user
+        msg["To"] = Header("被选召的孩子们", 'utf-8')
+
+        smtp = smtplib.SMTP(host=host, port=port)
+        smtp.ehlo(host)
+        smtp.login(user, password)
+
+        smtp.send_message(msg, user, recieve_email)
+        smtp.quit()
+        return True
+    except Exception:
+        return False
--- book/job/task.py
+++ book/job/task.py
@@ -1,5 +1,5 @@
 import os
-from book.db import redisutil, dbutil
+from book.db import redisutil, dbutil, mailutil
 
 cmd = './book_spider.sh'
 
@@ -15,9 +15,19 @@ def clean_job():
 def push_every_day_books():
     books = redisutil.smembers()
     users = dbutil.get_all_able_user()
-    if not users:
+    if not users or not books:
         return
     # 对books进行推送
-     # 推送邮件
-     # 推送短信
-    
+    # 推送邮件
+    send_mail = '18888106880@163.com'
+    message = get_message(books)
+    map(lambda x: mailutil.push_email(send_mail, x['email'], message), users)
+    # 推送短信
+
+
+def get_message(books):
+    message = '有你关注的类型书籍上线了哦，列表如下:'
+    for book in books:
+        book_name = '书名:{},'.format(book['book_name'])
+        message = message + book_name
+    return message
